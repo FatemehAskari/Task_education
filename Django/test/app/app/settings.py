5fc4e21187e53331547d6c4179fffa3f0ba3c860
FatemehAsakri <fatemeaskari2000@yahoo.com>
2024-07-07 15:10:24 +0330

    project app django

--- Django/test/app/app/settings.py
+++ Django/test/app/app/settings.py
@@ -38,8 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'application',
-    'comment',
-    'person'
 ]
 
 MIDDLEWARE = [
@@ -128,3 +126,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS = [BASE_DIR / "static"]
--- Django/test/app/application/admin.py
+++ Django/test/app/application/admin.py
@@ -3,3 +3,5 @@ from .models import *
 # Register your models here.
 
 admin.site.register(application)
+admin.site.register(person)
+admin.site.register(comment)
